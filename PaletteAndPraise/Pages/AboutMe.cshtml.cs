f60a406f83eeed936c45d2f281dc9ed9dd0b6dc6
Walt Beaman <walter.g.beaman@outlook.com>
2023-06-03 19:37:49 -0500

    Initial commit.

---
+++ PaletteAndPraise/Pages/AboutMe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PaletteAndPraise.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
